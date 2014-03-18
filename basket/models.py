92bfc9da4c27ace1ac15a7fc730eb26ff396651c
Iurii Kudriavtsev <iurii.kudriavtsev@txtr.com>
2014-03-18 17:06:05 +0100

    SKINSTXTRCOM-4447 | SKINSTXTRCOM-4163 | Using new API wrapper for basket handling in `user_settings` plugin.

--- basket/models.py
+++ basket/models.py
@@ -265,6 +265,10 @@ class Basket(Store, RpcMixin):
         return cls.signature(method='getFreeBasket', args=[token, marker])
 
     @classmethod
+    def get_validation(cls, token, marker=None):
+        return cls.signature(method='getValidationBasket', args=[token, marker])
+
+    @classmethod
     def checkout(cls, token, basket_id, method_preference, checkout_props):
         return cls.signature(method='checkoutBasket', data_converter=CheckoutResult, args=[token, basket_id, method_preference, checkout_props], deprecated='checkoutBasketAsynchronously')
 
