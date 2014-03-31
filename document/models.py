51516c9927a80a5592caeeb0fc908a5a7b7ad037
Iurii Kudriavtsev <iurii.kudriavtsev@txtr.com>
2014-03-31 15:32:40 +0200

    Adding a default value to document publication date field.

--- document/models.py
+++ document/models.py
@@ -31,7 +31,7 @@ class Document(Store, RpcMixin):
         language = Field(target='language')
         pages = IntField(target='number_of_pages')
         price = FloatField(target='price')
-        publication_date = DateField(target='publication_date')
+        publication_date = DateField(target='publication_date', default='')
         publication_status = DateField(target='publication_status')
         publisher = Field(target='publisher')
         size = IntField(target='size')
