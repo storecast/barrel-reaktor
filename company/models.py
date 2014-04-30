5f4cf0290d562cdb3041fa4af86490cd932c1aaa
Aurélien Scoubeau <aurelien.scoubeau@txtr.com>
2014-04-30 10:27:08 +0200

    SKINSTXTRCOM-4163 | Add barrel company models

---
+++ company/models.py
@@ -0,0 +1,41 @@
+from apps.barrel import Store, Field, BooleanField, EmbeddedStoreField, IntField, SplitField
+from apps.barrel.rpc import RpcMixin
+
+
+class PasswordPolicy(Store):
+    min_length = Field(target='minimumLength')
+    number_char_classes = Field(target='requiredNumberOfCharacterClasses')
+    supported_char_classes = SplitField(target='supportedCharacterClasses')
+    rules = SplitField(target='rules')
+
+
+class Nature(Store, RpcMixin):
+    interface = 'WSReaktorMgmt'
+
+    name = Field(target='name')
+    account_deletion_confirmation = BooleanField(target='enableAccountDeletionConfirmation')
+    auth_hash_method = Field(target='authenticationHashAlgorithm')
+    email_change_confirmation = BooleanField(target='enableEmailChangeConfirmation')
+    facebook_key = Field(target='facebookConfiguration:applicationKey')
+    facebook_permissions = Field(target='facebookConfiguration:profilePermissions')
+    home_country = Field(target='homeCountry')
+    lockout_attempts = IntField(target='loginAttemptsBeforeLockout')
+    lockout_duration = IntField(target='lockoutPeriodMinutes')
+    shop_currency = Field(target='shopCurrency')
+    shop_url = Field(target='shopUrl')
+    password_policy = EmbeddedStoreField(target='passwordPolicy', store_class=PasswordPolicy)
+
+    @classmethod
+    def get_by_name(cls, name):
+        return cls.signature(method='getNature', args=[name])
+
+
+class Company(Store, RpcMixin):
+    interface = 'WSReaktorMgmt'
+
+    name = Field(target='name')
+    natures = EmbeddedStoreField(target='natures', store_class=Nature, is_array=True)
+
+    @classmethod
+    def get_by_name(cls, name):
+        return cls.signature(method='getCompany', args=[name])
--- models.py
+++ models.py
@@ -1,17 +1,4 @@
 from apps.barrel import Store, Field, FloatField
-from apps.barrel.rpc import RpcMixin
-
-
-class Nature(Store, RpcMixin):
-    interface = 'WSReaktorMgmt'
-
-    name = Field(target='name')
-    home_country = Field(target='homeCountry')
-    auth_hash_method = Field(target='authenticationHashAlgorithm')
-
-    @classmethod
-    def load(cls, name):
-        return cls.signature(method='getNature', args=[name])
 
 
 class Price(Store):
