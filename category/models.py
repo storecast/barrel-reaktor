ec7112d0cc1a9b0daf9b6cfd0de8ebc4d5888988
Aurélien Scoubeau <aurelien.scoubeau@txtr.com>
2014-05-28 11:41:20 +0200

    SKNS-4163 | Add some calls to barrel category models.

--- category/models.py
+++ category/models.py
@@ -1,6 +1,11 @@
 from apps.barrel import Store, Field, IntField
-from apps.barrel.rpc import RpcMixin
 from apps.barrel.cache import cache, sliced_call_args
+from apps.barrel.rpc import RpcMixin
+
+
+def document_converter(*args, **kwargs):
+    from apps.reaktor_barrel.document.models import Document
+    return Document(*args, **kwargs)
 
 
 class Category(Store, RpcMixin):
@@ -18,10 +23,20 @@ class Category(Store, RpcMixin):
 
     @classmethod
     @cache(keygen=sliced_call_args(i=1), need_cache=lambda cat: False)  # ready to be enabled
-    def get_by_id(cls, token, cat_id, with_children=True, sort=None, direction='asc', offset='0', number_of_results=-1):
+    def get_by_id(cls, token, cat_id, with_children=True, offset='0', number_of_results=-1, sort=None, direction='asc'):
         invert = direction == 'desc'
-        return cls.signature(method='getContentCategory', args=[token, cat_id, with_children, sort, invert, offset, number_of_results])
+        return cls.signature(method='getContentCategory',
+                             args=[token, cat_id, with_children, sort, invert, offset, number_of_results])
 
     @classmethod
     def get_roots_by_token(cls, token, depth=0, min_number_of_documents=0):
-        return cls.signature(method='getCatalogContentCategoryRootsForUser', args=[token, depth, min_number_of_documents])
+        return cls.signature(method='getCatalogContentCategoryRootsForUser',
+                             args=[token, depth, min_number_of_documents])
+
+    @classmethod
+    @cache(keygen=sliced_call_args(i=1), need_cache=lambda docs: False)  # ready to be enabled
+    def get_documents(cls, token, cat_id, include_sub_cats=False, offset=0, number_of_results=-1, sort=None, direction='asc'):
+        invert = direction == 'desc'
+        return cls.signature(interface='WSDocMgmt', method='getDocumentsInContentCategory',
+                             args=[token, cat_id, include_sub_cats, sort, invert, offset, number_of_results],
+                             data_converter=document_converter)
