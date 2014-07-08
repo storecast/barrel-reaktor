6ffd43c1db29603a8b3cb13d8f6edf5091d3dc67
Aurélien Scoubeau <aurelien.scoubeau@txtr.com>
2014-07-08 16:45:03 +0200

    Update setup.py
    Bump version.

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='barrel-reaktor',
-    version='0.0.1-dev',
+    version='0.0.1',
     description='python interface to reaktor API',
     long_description=__doc__,
     license='BSD',
@@ -13,9 +13,9 @@ setup(
     url='https://github.com/txtr/barrel-reaktor/',
     packages=find_packages(),
     platforms='any',
-    install_requires=['barrel==0.0.1-dev', 'python-money',],
+    install_requires=['barrel', 'python-money', ],
     dependency_links=[
-        'https://github.com/txtr/barrel/zipball/master#egg=barrel-0.0.1-dev',
+        'https://github.com/txtr/barrel/zipball/master#egg=barrel',
         'https://github.com/txtr/python-money/zipball/master#egg=python-money',
     ]
 )
