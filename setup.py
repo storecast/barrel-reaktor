4821c562fe85b046ba1f9d9b3e179742ab43c3c3
Aurélien Scoubeau <aurelien.scoubeau@txtr.com>
2014-08-19 17:11:19 +0200

    Bump version to 0.2.1

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='barrel-reaktor',
-    version='0.2.0',
+    version='0.2.1',
     description='python interface to reaktor API',
     long_description=__doc__,
     license='BSD',
