8ad2daf87251be33ccac452fe18590b5949d0350
bjoern larsson <bjoern.larsson@txtr.com>
2014-10-21 13:02:04 +0200

    SKNS-5116 - added required methods for voucher management

--- barrel_reaktor/voucher/models.py
+++ barrel_reaktor/voucher/models.py
@@ -1,4 +1,4 @@
-from barrel import Store, Field, IntField, EmbeddedStoreField
+from barrel import Store, Field, DateField, IntField, EmbeddedStoreField
 from barrel.rpc import RpcMixin
 from barrel_reaktor.models import Price
 from money import Money
@@ -7,13 +7,16 @@ from money import Money
 # FIXME: this class is used as embedded for `voucherApplications` as well as for `Voucher` - a gift card
 # thus it might differ from the voucher that comes in the basket position (gift card)
 class Voucher(Store, RpcMixin):
+    interface = 'WSVoucherMgmt'
+    
     code = Field(target='code')
     text = Field(target='text')
     percentage = IntField(target='percentage')
+    valid_from = DateField(target='validFrom')
+    expiration_date = DateField(target='expirationDate')
     _initial_amount = EmbeddedStoreField(target='initialAmount', store_class=Price)
     _amount = EmbeddedStoreField(target='amount', store_class=Price)
-    # not sure if it is needed
-    # java_cls = Field(target='javaClass')
+    _java_cls = Field(target='javaClass')
 
     @property
     def initial_amount(self):
@@ -22,3 +25,15 @@ class Voucher(Store, RpcMixin):
     @property
     def amount(self):
         return Money(amount=self._amount.amount, currency=self._amount.currency)
+    
+    @property
+    def is_amount(self):
+        return "WSTAmountVoucher" in self._java_cls
+    
+    @property
+    def is_percent(self):
+        return "WSTPercentVoucher" in self._java_cls
+    
+    @classmethod
+    def get(cls, token):
+        return cls.signature(method='getAssignedVouchers', args=[token])
