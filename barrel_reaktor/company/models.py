e2555b68f2448e9dfaaea5b3c7c507bbda638c5b
Aurélien Scoubeau <aurelien.scoubeau@txtr.com>
2015-01-08 09:50:58 +0100

    Add address format to company nature model.

--- barrel_reaktor/company/models.py
+++ barrel_reaktor/company/models.py
@@ -12,10 +12,17 @@ class PasswordPolicy(Store):
 class Nature(Store, RpcMixin):
     interface = 'WSReaktorMgmt'
 
+    class AddressFormat(Store):
+        fmt = Field(target='fmt')
+        require = Field(target='require')
+        zip = Field(target='zip')
+
     name = Field(target='name')
-    available_in_multistore_app = BooleanField(target='availableInMultistoreApp')
     account_deletion_confirmation = BooleanField(target='enableAccountDeletionConfirmation')
+    address_format = EmbeddedStoreField(target='addressFormat',
+                                        store_class=AddressFormat)
     auth_hash_method = Field(target='authenticationHashAlgorithm')
+    available_in_multistore_app = BooleanField(target='availableInMultistoreApp')
     email_change_confirmation = BooleanField(target='enableEmailChangeConfirmation')
     facebook_key = Field(target='facebookConfiguration:applicationKey')
     facebook_permissions = Field(target='facebookConfiguration:profilePermissions')
@@ -23,9 +30,10 @@ class Nature(Store, RpcMixin):
     lockout_attempts = IntField(target='loginAttemptsBeforeLockout')
     lockout_duration = IntField(target='lockoutPeriodMinutes')
     mobi_conversion = Field(target='mobiConversionEnabled')
+    password_policy = EmbeddedStoreField(target='passwordPolicy',
+                                         store_class=PasswordPolicy)
     shop_currency = Field(target='shopCurrency')
     shop_url = Field(target='shopUrl')
-    password_policy = EmbeddedStoreField(target='passwordPolicy', store_class=PasswordPolicy)
 
     @classmethod
     def get_by_name(cls, name):
@@ -36,7 +44,8 @@ class Company(Store, RpcMixin):
     interface = 'WSReaktorMgmt'
 
     name = Field(target='name')
-    natures = EmbeddedStoreField(target='natures', store_class=Nature, is_array=True)
+    natures = EmbeddedStoreField(target='natures', store_class=Nature,
+                                 is_array=True)
 
     @classmethod
     def get_by_name(cls, name):
