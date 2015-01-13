48096bcabe9b214e30aa65fafe687935c6b4ffaa
Aurélien Scoubeau <aurelien.scoubeau@txtr.com>
2015-01-13 15:35:25 +0100

    Alias document.attributes for simpler nested attributes access.

--- barrel_reaktor/document/models.py
+++ barrel_reaktor/document/models.py
@@ -11,46 +11,6 @@ class Document(Store, RpcMixin):
         first_name = Field(target='firstName')
         last_name = Field(target='lastName')
 
-    class Attributes(Store):
-        audience = Field(target='audience')
-        author = Field(target='author', default=u'')
-        author_bio = Field(target='author_biography', default='')
-        catalog_id = Field(target='catalog_document_id')
-        content_provider_id = Field(target='content_provider_specific_id')
-        content_provider_name = Field(target='content_provider_name')
-        content_source_id = Field(target='content_source_id')
-        cover_ratio = FloatField(target='cover_image_aspect_ratio')
-        cover_type = Field(target='cover_image_type')
-        currency = Field(target='currency')
-        description = Field(target='description')
-        editors_comment = Field(target='editors_comment')
-        extract = Field(target='extract')
-        first_publication = Field(target='date_of_first_publication')
-        fulfillment_id = Field(target='fulfillment_id')
-        fulfillment_type = Field(target='fulfillment_type')
-        hash = Field(target='binary_hash')
-        _isbn = Field(target='isbn')
-        imprint = Field(target='imprint', default='')
-        language = Field(target='language')
-        large_cover_url = Field(target='cover_image_url_large')
-        medium_cover_url = Field(target='cover_image_url_medium')
-        normal_cover_url = Field(target='cover_image_url_normal')
-        pages = IntField(target='number_of_pages')
-        price = FloatField(target='price')
-        publication_date = DateField(target='publication_date')
-        publication_status = DateField(target='publication_status')
-        publisher = Field(target='publisher')
-        size = IntField(target='size')
-        subtitle = Field(target='subtitle')
-        tax_group = Field(target='tax_group')
-        title = Field(target='title', default=u'')
-        undiscounted_price = FloatField(target='undiscounted_price')
-        year = IntField(target='year')
-
-        @property
-        def isbn(self):
-            return self._isbn.strip()
-
     class License(Store):
         key = Field(target='key')
         user_roles = Field(target='currentUserRoles')
@@ -58,42 +18,89 @@ class Document(Store, RpcMixin):
     class Preview(Store):
         format = Field(target='format')
 
-    id = Field(target='documentID')
-    attributes = EmbeddedStoreField(target='attributes', store_class=Attributes)
-    authors = EmbeddedStoreField(target='authors', store_class=Author, is_array=True)
+    _categories = EmbeddedStoreField(
+        target='contentCategories',
+        store_class='barrel_reaktor.category.models.Category',
+        is_array=True)
+    _isbn = Field(target='attributes:isbn')
+    audience = Field(target='attributes:audience')
+    author = Field(target='attributes:author', default=u'')
+    author_bio = Field(target='attributes:author_biography', default='')
+    authors = EmbeddedStoreField(target='authors',
+                                 store_class=Author, is_array=True)
+    catalog_id = Field(target='attributes:catalog_document_id')
     catalog_state = Field(target='catalogDocumentState')
-    _categories = EmbeddedStoreField(target='contentCategories', store_class='barrel_reaktor.category.models.Category', is_array=True)
     category_ids = Field(target='contentCategoryIDs')
+    content_provider_id = Field(
+        target='attributes:content_provider_specific_id')
+    content_provider_name = Field(target='attributes:content_provider_name')
+    content_source_id = Field(target='attributes:content_source_id')
+    cover_ratio = FloatField(target='attributes:cover_image_aspect_ratio')
+    cover_type = Field(target='attributes:cover_image_type')
     creation_date = DateField(target='creationTime')
+    cumulative_votes = IntField(target='cumulativeVotes:stars', default=0)
+    currency = Field(target='attributes:currency')
     delivery_url = Field(target='deliveryUrl')
+    description = Field(target='attributes:description')
     drm_type = Field(target='drm')
+    editors_comment = Field(target='attributes:editors_comment')
+    extract = Field(target='attributes:extract')
     file_name = Field(target='fileName', default='')
+    first_publication = Field(target='attributes:date_of_first_publication')
     format = Field(target='format', default='')
+    fulfillment_id = Field(target='attributes:fulfillment_id')
+    fulfillment_type = Field(target='attributes:fulfillment_type')
     has_binary = BooleanField(target='hasBinary')
     has_thumbnail = BooleanField(target='hasThumbnail')
+    hash = Field(target='attributes:binary_hash')
+    id = Field(target='documentID')
+    imprint = Field(target='attributes:imprint', default='')
     lang_code = Field(target='languageCode')
-    licenses = EmbeddedStoreField(target='licenses', store_class=License, is_array=True)
+    language = Field(target='attributes:language')
+    large_cover_url = Field(target='attributes:cover_image_url_large')
+    licenses = EmbeddedStoreField(target='licenses',
+                                  store_class=License, is_array=True)
+    medium_cover_url = Field(target='attributes:cover_image_url_medium')
     modification_date = DateField(target='modificationTime')
     name = Field(target='displayName')
+    normal_cover_url = Field(target='attributes:cover_image_url_normal')
     owner = Field(target='owner')
-    previews = EmbeddedStoreField(target='documentPreviews', store_class=Preview, is_array=True)
+    pages = IntField(target='attributes:number_of_pages')
+    personal_votes = IntField(target='personalVotes:stars', default=0)
+    previews = EmbeddedStoreField(target='documentPreviews',
+                                  store_class=Preview, is_array=True)
+    price = FloatField(target='attributes:price')
+    publication_date = DateField(target='attributes:publication_date')
+    publication_status = DateField(target='attributes:publication_status')
+    publisher = Field(target='attributes:publisher')
+    size = IntField(target='attributes:size')
+    subtitle = Field(target='attributes:subtitle')
+    tax_group = Field(target='attributes:tax_group')
+    title = Field(target='attributes:title', default=u'')
+    title = Field(target='attributes:title', default=u'')
     type = Field(target='type')
+    undiscounted_price = FloatField(target='attributes:undiscounted_price')
     user_state = Field(target='userDocumentState', default='?')
     user_tags = Field(target='userTags', default=[])
     version = IntField(target='version')
     version_access_type = Field(target='versionAccessType')
     version_size = IntField(target='versionSize')
     votes = IntField(target='numberOfVotes')
-    cumulative_votes = IntField(target='cumulativeVotes:stars', default=0)
-    personal_votes = IntField(target='personalVotes:stars', default=0)
+    year = IntField(target='attributes:year')
+
+    @property
+    def isbn(self):
+        return self._isbn.strip()
 
     @property
     def price(self):
-        return Money(amount=self.attributes.price, currency=self.attributes.currency)
+        return Money(amount=self.attributes.price,
+                     currency=self.attributes.currency)
 
     @property
     def undiscounted_price(self):
-        return Money(amount=self.attributes.undiscounted_price, currency=self.attributes.currency)
+        return Money(amount=self.attributes.undiscounted_price,
+                     currency=self.attributes.currency)
 
     @property
     def is_preorder(self):
@@ -134,10 +141,11 @@ class Document(Store, RpcMixin):
 
     @property
     def categories(self):
-        """Builds a list of categories in tree order, from oldest ancestor to the leaf.
-        It relies on both `category_ids` and `_categories` attributes.
-        Note that according to reaktor, when viewing a document from a catalog that is not
-        associated to the token nature, the information is not available.
+        """Builds a list of categories in tree order, from oldest ancestor
+        to the leaf. It relies on both `category_ids` and `_categories`
+        attributes. Note that according to reaktor, when viewing a document
+        from a catalog that is not associated to the token nature,
+        the information is not available.
         """
         if hasattr(self, '_category_trail'):
             return self._category_trail
@@ -170,17 +178,24 @@ class Document(Store, RpcMixin):
     @classmethod
     def get_user_doc_id(cls, token, doc_id):
         """Returns user document id for the catalog document id if any."""
-        return cls.signature(method='getUserDocumentID', data_converter=lambda d: d, args=[token, doc_id])
+        return cls.signature(
+            method='getUserDocumentID',
+            data_converter=lambda d: d, args=[token, doc_id])
 
     @classmethod
     def get_doc_path(cls, token, doc_id, is_user=False):
-        """Returns the path to unzipped epub user document or catalog preview."""
+        """Returns the path to unzipped epub user document or catalog
+        preview.
+        """
         method = 'unzipEpubUserDocument' if is_user else 'unzipEpubPreview'
-        return cls.signature(method=method, data_converter=lambda d: d, args=[token, doc_id])
+        return cls.signature(method=method, data_converter=lambda d: d,
+                             args=[token, doc_id])
 
     @classmethod
     def get_by_isbn(cls, token, isbn):
-        """Returns a document by isbn, using search API endpoint since fetching doc by isbn requires extra rights."""
+        """Returns a document by isbn, using search API endpoint since
+        fetching doc by isbn requires extra rights.
+        """
         def converter(data):
             if 'results' in data:
                 return Document(data['results'][0]['searchResult'])
@@ -188,18 +203,22 @@ class Document(Store, RpcMixin):
             # raise a proper exception in this case
             else:
                 raise ReaktorArgumentError
-        args = [token, 'isbn:%s' % isbn, None, 0, 1, None, False, None, None, {'resultType': 'Object'}]
+        args = [token, 'isbn:%s' % isbn, None, 0, 1, None, False, None, None,
+                {'resultType': 'Object'}]
         return cls.signature(
-            interface="WSSearchDocument", method='searchDocuments', data_converter=converter, args=args)
+            interface="WSSearchDocument", method='searchDocuments',
+            data_converter=converter, args=args)
 
     @classmethod
     def get_related_by_id(cls, token, doc_id, offset=0, number_of_results=5):
         """Returns a list of `Documents` that are related to the given id."""
-        return cls.signature(method='getDocumentsRelatedToDocument', args=[token, doc_id, offset, number_of_results])
+        return cls.signature(method='getDocumentsRelatedToDocument',
+                             args=[token, doc_id, offset, number_of_results])
 
     @classmethod
     def change_attributes(cls, token, doc_ids, attributes):
-        return cls.signature(method='changeDocumentAttributes', args=[token, doc_ids, attributes])
+        return cls.signature(method='changeDocumentAttributes',
+                             args=[token, doc_ids, attributes])
 
     @classmethod
     def remove_cover(cls, token, doc_id):
